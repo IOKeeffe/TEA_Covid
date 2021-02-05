cb239c58b0f11bf88258252a2a731cf838b7184d
Ian O'Keeffe <iokeef@gmail.com>
2021-02-04 23:19:49 -0500

    add houston report

--- tea_reports.R
+++ tea_reports.R
@@ -3,16 +3,15 @@ library(ggplot2)
 tea_data <- read.csv("artf354001_Part2_District_Crisis.csv")
 large_districts <- list(246909, 61902, 43910, 101917, 101915, 57909, 71902, 220901, 60902, 43905, 101902, 15910, 79907, 220905, 101914, 227901, 101907, 57905, 244905, 101912)
 # tea_data <- as.numeric(gsub(",","",STUDENT.COUNT))
-# cleaned_data <- tea_data %>%
-#   transform(STUDENT.COUNT = as.numeric(gsub(",","",STUDENT.COUNT))) %>%
-#   filter(!is.na(STUDENT.COUNT)&STUDENT.COUNT != -999) %>%
-#   filter(DISTRICT %in% large_districts)
+cleaned_data <- tea_data %>%
+  transform(STUDENT.COUNT = as.numeric(gsub(",","",STUDENT.COUNT))) %>%
+  filter(!is.na(STUDENT.COUNT)&STUDENT.COUNT != -999) %>%
+  filter(DISTRICT %in% large_districts)
 
 houston <- tea_data %>%
   filter(DISTRICT.NAME == "HOUSTON ISD") %>%
-  transform(STUDENT.COUNT = as.numeric(gsub(",","",STUDENT.COUNT)))
-  # transform(STUDENT.COUNT = as.numeric(STUDENT.COUNT))
-  # mutate(Percent = (STUDENT.COUNT/sum(STUDENT.COUNT) * 100))
+  transform(STUDENT.COUNT = as.numeric(gsub(",","",STUDENT.COUNT))) %>%
+  mutate(Percent = (STUDENT.COUNT/sum(STUDENT.COUNT) * 100))
 
 district_percentages <- cleaned_data %>%
   group_by(DISTRICT.NAME) %>%
