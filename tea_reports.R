5665103c8157f090644608a5bff05beef7c1ff6d
Ian O'Keeffe <iokeef@yahoo.com>
2021-02-23 12:12:54 -0500

    collapse groups, add function to find extremes

--- tea_reports.R
+++ tea_reports.R
@@ -3,30 +3,10 @@ library(ggplot2)
 tea_data <- read.csv("artf354001_Part2_District_Crisis.csv")
 large_districts <- list(246909, 61902, 43910, 101917, 101915, 57909, 71902, 220901, 60902, 43905, 101902, 15910, 79907, 220905, 101914, 227901, 101907, 57905, 244905, 101912)
 
-calculate_score <- function(row) {
-  if(row$CRISIS.CODE == "7A") {
-    4 * STUDENT.COUNT
-  } else if(row$CRISIS.CODE %in% list('7D', '7G')) {
-    3 * STUDENT.COUNT
-  } else if(row$CRISIS.CODE %in% list('7C', '7F', '7H', '7I')) {
-    2 * STUDENT.COUNT
-  } else if(row$CRISIS.CODE == "7E") {
-    1 * STUDENT.COUNT
-  } else if(row$CRISIS.CODE == "7B") {
-    0 * STUDENT.COUNT
-  }
-  return(score * row$STUDENT.COUNT)
-}
-
 cleaned_data <- tea_data %>%
   transform(STUDENT.COUNT = as.numeric(gsub(",","",STUDENT.COUNT))) %>%
   filter(STUDENT.COUNT != -999) %>%
   filter(DISTRICT %in% large_districts)
-
-houston <- tea_data %>%
-  filter(DISTRICT.NAME == "HOUSTON ISD") %>%
-  transform(STUDENT.COUNT = as.numeric(gsub(",","",STUDENT.COUNT))) %>%
-  mutate(Percent = (STUDENT.COUNT/sum(STUDENT.COUNT) * 100))
 
 district_percentages <- cleaned_data %>%
   group_by(DISTRICT) %>%
@@ -40,19 +20,30 @@ scored_districts <- cleaned_data %>%
     CRISIS.CODE %in% list('7D', '7G') ~ 3 * STUDENT.COUNT,
     CRISIS.CODE %in% list('7C', '7F', '7H', '7I') ~ 2 * STUDENT.COUNT,
     CRISIS.CODE == "7E" ~ 1 * STUDENT.COUNT,
-    CRISIS.CODE == "7B" ~ 0)
-  ) %>%
+    CRISIS.CODE == "7B" ~ 0)) %>%
   group_by(DISTRICT) %>%
   mutate(TOTAL.CRISIS.SCORE = sum(CRISIS.SCORE)) %>%
   mutate(MAX.CRISIS.SCORE = sum(STUDENT.COUNT) * 4) %>%
   mutate(CRISIS_GRADE = TOTAL.CRISIS.SCORE / MAX.CRISIS.SCORE) %>%
-  summarize(DISTRICT.NAME, TOTAL.CRISIS.SCORE, MAX.CRISIS.SCORE, CRISIS_GRADE) %>% collapse
+  summarize(DISTRICT.NAME, TOTAL.CRISIS.SCORE, MAX.CRISIS.SCORE, CRISIS_GRADE, .groups = "keep") %>%
+  distinct()
 
-# Top 3: 1: Frisco, 2: Katy, 3: Lewisville
-# Bottom 3: Aldine, Houston, Arlington
 
-  # arrange(sum(CRISIS.SCORE))
-  # aggregate(CRISIS.SCORE, list(district = DISTRICT), sum)
+  # Top 3: 1: Frisco, 2: Katy, 3: Lewisville
+  # Bottom 3: Aldine, Houston, Arlington
+find_extremes <- function(code) {
+  return(district_percentages %>%
+    filter(CRISIS.CODE == code))
+}
+fully_engaged <- find_extremes("7A")
+# Top 3 Engagement (7A) -> 1: Frisco, 2: Katy, 3: Lewisville
+# Bottom 3 Engagement (7A) -> 1(worst): Aldine, 2: Houston, 3: Arlington
+
+no_contact <- find_extremes("7B")
+# Top 3 No Contact (7B) -> 1: Frisco 2: Cypress-Fairbanks 3: Lewisville
+# Bottom 3 No Contact (7B) -> 1(worst): Aldine, 2: Houston 3: Fort Worth
+
+  
 
 # This creates a plot but it is UGLY
 # ggplot(data=district_percentages, aes(x=DISTRICT.NAME, y=Pct, fill=CRISIS.CODEX)) +
